600579c302efcdbdc51f0742fc927b6cef987a1e
Saul Chelewani <saul@patkay.net>
2021-05-05 09:44:06 +0200

    Added Msisdn class

---
+++ src/Msisdn.php
@@ -0,0 +1,51 @@
+<?php
+
+
+class Msisdn
+{
+    private string $msisdn;
+
+    public function __construct(string $msisdn)
+    {
+        $this->msisdn = $this->parse($msisdn);
+    }
+
+    private function parse(string $msisdn): string
+    {
+        return substr(preg_replace('/\D/', '', $msisdn), -9);
+    }
+
+    public function validTnmNumber(): bool
+    {
+        return $this->matchesPattern("/^(0)(((88)\d{7})|(31\d{7}))$/");
+    }
+
+    private function matchesPattern(string $pattern): bool
+    {
+        try {
+            return preg_match($pattern, $this->msisdn);
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+
+    public function toCbsFormat(): string
+    {
+        return $this->msisdn;
+    }
+
+    public function humanized(): string
+    {
+        return '0' . $this->msisdn;
+    }
+
+    public function toString(): string
+    {
+        return $this->msisdn;
+    }
+
+    public function internationalized(): string
+    {
+        return '265' . $this->msisdn;
+    }
+}
--- src/helpers.php
+++ src/helpers.php
@@ -1,23 +1,23 @@
 <?php
 
 if (!function_exists('vgs_phone_number')) {
-    function vgs_phone_number($phone_number)
+    function vgs_phone_number($phone_number): string
     {
-        return '265' . subscriber_number($phone_number);
+        return msisdn($phone_number)->internationalized();
     }
 }
 
 if (!function_exists('humanized_phone_number')) {
     function humanized_phone_number($phone_number): string
     {
-        return '0' . subscriber_number($phone_number);
+        return msisdn($phone_number)->humanized();
     }
 }
 
 if (!function_exists('subscriber_number')) {
     function subscriber_number($phone_number)
     {
-        return substr(preg_replace('/\D/', '', $phone_number), -9);
+        return msisdn($phone_number)->toString();
     }
 }
 
@@ -36,19 +36,23 @@ if (!function_exists('generate_key')) {
 }
 
 if (!function_exists('is_valid_tnm_number')) {
-    function is_valid_tnm_number(string $number)
+    function is_valid_tnm_number(string $number): bool
     {
-        try {
-            return preg_match("/^(0)(((88)\d{7})|(31\d{7}))$/", humanized_phone_number($number));
-        } catch (Exception $e) {
-            return false;
-        }
+        return msisdn($number)->validTnmNumber();
     }
 }
 
 if (!function_exists('cbs_phone_number')) {
     function cbs_phone_number(string $msisdn): string
     {
-        return subscriber_number($msisdn);
+        return msisdn($msisdn)->toCbsFormat();
+    }
+}
+
+
+if (!function_exists('msisdn')) {
+    function msisdn(string $msisdn) : Msisdn
+    {
+        return new Msisdn($msisdn);
     }
 }
